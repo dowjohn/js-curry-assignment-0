9d86a8db91eafe72cff836ec23d0c75593b44478
John Dow <jldjld503@gmail.com>
2017-03-02 16:18:37 -0600

    Finished assignment using the provided functional methods

--- lib.js
+++ lib.js
@@ -28,18 +28,32 @@ const cart =
  */
 const itemRepeater =
   itemName =>
-    count => {
-      // TODO
-    }
+    count => new Array(count).fill(itemName)
 
 /**
  * should return an array of carts with each given customer's shopping list
  * as an array of items
+ * [{
+ *  customer: 'charles',
+ *  items: ['celery', 'celery', 'taco']
+ * },{
+ *  customer: 'charles',
+ *  items: ['celery', 'celery', 'taco']
+ * }]
  */
 const constructCarts =
   listings =>
     customers => {
-      // TODO
+      return customers.map(
+        (customerObject) => {
+          return {
+            customer: customerObject.name,
+            items: entries(customerObject.shoppingList).reduce(
+              (previous, keyPair) => {
+                return previous.concat(itemRepeater(keyPair[0])(keyPair[1]))
+              }, [])
+          }
+        })
     }
 
 module.exports = {
